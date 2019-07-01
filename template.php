84e09c6e8563ed20e657b5e67526a2da30c11566
eMul <e2016141010@gmail.com>
2019-07-02 02:09:05 +0700

    revisi 1

--- template.php
+++ template.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>title</title>
     <link rel="stylesheet" href="assets/css/bootsrap.min.css">
+    <title>title</title>
 </head>
 <body>
     
