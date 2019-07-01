748881ba6626d252171a1c761d66dafd18548f36
eMul <e2016141010@gmail.com>
2019-07-01 12:44:13 +0700

    add jquery

---
+++ template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>title</title>
+    <link rel="stylesheet" href="assets/css/bootsrap.min.css">
+</head>
+<body>
+    
+<h1>Hallo word!!!</h1>
+
+
+
+<!-- js file--->
+<script src="assets/js/jquery-slim.min.js"> </script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
