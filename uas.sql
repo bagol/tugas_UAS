2c50217133c321144da5519bf67bea0f503cba9b
eMul <e2016141010@gmail.com>
2019-07-02 23:05:50 +0700

    update database

--- uas.sql
+++ uas.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 01, 2019 at 12:35 PM
+-- Generation Time: Jul 02, 2019 at 11:04 PM
 -- Server version: 10.0.38-MariaDB-0ubuntu0.16.04.1
--- PHP Version: 7.2.17-1+ubuntu16.04.1+deb.sury.org+3
+-- PHP Version: 7.2.19-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -57,13 +57,6 @@ CREATE TABLE `jadwal` (
   `jam_selesai` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `jadwal`
---
-
-INSERT INTO `jadwal` (`id`, `kelas`, `id_mapel`, `hari`, `jam_mulai`, `jam_selesai`) VALUES
-(1, '7B', 1, 'Senin', '07:30:00', '09:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -99,7 +92,10 @@ CREATE TABLE `mapel` (
 --
 
 INSERT INTO `mapel` (`id`, `nip`, `nama`) VALUES
-(1, '20290621001', 'matematika');
+(1, '20290621001', 'matematika'),
+(2, '2016140993', 'Bahasa Indonesia'),
+(3, '2016140993', 'Bahasa Inggris'),
+(4, '20290621001', 'fisika');
 
 -- --------------------------------------------------------
 
@@ -121,7 +117,7 @@ CREATE TABLE `murid` (
 --
 
 INSERT INTO `murid` (`nis`, `nama`, `password`, `alamat`, `email`, `notlpn`) VALUES
-('2016141010', 'mulyadi', '$2y$10$CgARxjFDYXeHB3fd.s8tb.hV9U0ga.khkuA/Pcmo0jHT0/aLAibde', 'depok', 'e2016141010@gmail.com', '081222129621');
+('2016141010', 'mulyadih', '$2y$10$/qUxqLtVh8F7H02K94h.l.IDdPXWSGOPj41wevhDVJpajReHDEA4.', 'depok', 'e2016141010@gmail.com', '081222129621');
 
 -- --------------------------------------------------------
 
@@ -133,15 +129,18 @@ CREATE TABLE `nilai` (
   `id` int(11) NOT NULL,
   `nis` varchar(12) NOT NULL,
   `id_mapel` int(11) NOT NULL,
-  `nilai` int(11) NOT NULL
+  `tugas` int(11) NOT NULL,
+  `uts` int(3) NOT NULL,
+  `uas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `nilai`
 --
 
-INSERT INTO `nilai` (`id`, `nis`, `id_mapel`, `nilai`) VALUES
-(1, '2016141010', 1, 80);
+INSERT INTO `nilai` (`id`, `nis`, `id_mapel`, `tugas`, `uts`, `uas`) VALUES
+(1, '2016141010', 1, 75, 80, 80),
+(2, '2016141010', 2, 70, 75, 80);
 
 -- --------------------------------------------------------
 
@@ -161,6 +160,7 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`nip`, `nama`, `password`, `email`) VALUES
+('2016141044', 'Ibnu Fajar Yusuf', '$2y$10$1YvoceQDGOnQLlINYyUPBOqOu1tqtEwBwEQeT7uXqCGfTt9FdJJ9e', 'ify@ify.com'),
 ('201906230001', 'staff', '$2y$10$11hEfCD1VVoCr7EU5iMTh.EnoIuISc1J8S4jnv5Wm0Oc6kuv64Nly', 'staff@staff.com');
 
 --
@@ -207,7 +207,8 @@ ALTER TABLE `murid`
 ALTER TABLE `nilai`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_mapel` (`id_mapel`),
-  ADD KEY `nis` (`nis`);
+  ADD KEY `nis` (`nis`),
+  ADD KEY `nis_2` (`nis`);
 
 --
 -- Indexes for table `staff`
@@ -228,12 +229,12 @@ ALTER TABLE `jadwal`
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
